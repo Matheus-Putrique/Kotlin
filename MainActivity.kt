fb52c627b262156c12f9c803cb842172ae76c5d7
Matheus da Silveira Putrique <145862549+Matheus-Putrique@users.noreply.github.com>
2026-08-21 09:37:03 -0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -5,9 +5,15 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -21,6 +27,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -90,20 +99,44 @@ fun PrimeiraTelaPreview() {
 @Composable
 fun Formulario(){
 
+
+
     var nome by remember { mutableStateOf("") }
+    var senha by remember { mutableStateOf("") }
 
-    Column() {
-        Text("Cadastro")
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        shape = RoundedCornerShape(10.dp),
+        color = Color(255, 255, 0)
 
-        TextField(
-            value = nome,
-            onValueChange = {nome = it},
-            label = { Text("Nome") }
+    )  {
 
-        )
+        Column(
+            modifier = Modifier.padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Cadastro")
 
-        Button(onClick = {}) {
-            Text("Enviar")
+            TextField(
+                value = nome,
+                onValueChange = { nome = it },
+                label = { Text("Nome") }
+
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                value = senha,
+                onValueChange = { senha = it },
+                label = { Text("Senha") }
+
+            )
+
+            Button(onClick = {}) {
+                Text("Enviar")
+            }
         }
     }
 }
